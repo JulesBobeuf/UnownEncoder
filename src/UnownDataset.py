a451933f14254dbbc4b7ad7eed0df6c0d6fb8001
Bobeuf Jules <jules_bobeuf@ens.univ-artois.fr>
2024-03-02 23:31:31 +0100

    changed dataset, model = done

--- src/UnownDataset.py
+++ src/UnownDataset.py
@@ -1,24 +1,48 @@
-import pandas as pd
-from PIL import Image
+import torch
 import torchvision
+import numpy as np
 
 class UnownDataset(torchvision.datasets.ImageFolder):
-    def __init__(self, csv_file, root_dir, transform=None):
-        self.labels = pd.read_csv(csv_file)
-        self.root_dir = root_dir
+    def __init__(self, image_path, label_path, transform=None):
+        self.data = torch.from_numpy(np.load(image_path))
+        self.labels = torch.from_numpy(np.load(label_path)).long()
         self.transform = transform
 
     def __len__(self):
-        return len(self.labels)
+        return len(self.data)
 
     def __getitem__(self, idx):
-        img_name = self.labels.iloc[idx, 0]
-        img_path = f"{self.root_dir}/{img_name}"
-        image = Image.open(img_path).convert('L')  # 'L' mode for grayscale images
-
-        label = self.labels.iloc[idx, 1]  # Assuming label is in the second column of the CSV file
-
-        if self.transform:
-            image = self.transform(image)
-
-        return image, label
+        sample = self.data[idx].float()
+        sample = sample.unsqueeze(0)
+        sample = self.transform(sample)
+        return sample, translate_dict[chr(63+self.labels[idx].item())], self.labels[idx]
+    
+# for some reason indexes are delayed...
+translate_dict = {'A': 'O', 
+             'B': 'V', 
+             'C': 'B', 
+             'D': 'I', 
+             'E': 'P',
+             'F': 'W', 
+             'G': 'C', 
+             'H': 'J', 
+             'I': 'Q', 
+             'J': 'X',
+             'K': 'D', 
+             'L': 'K', 
+             'M': 'R', 
+             'N': 'Y', 
+             'O': 'E',
+             'P': 'L', 
+             'Q': 'S', 
+             'R': 'Z', 
+             'S': 'F', 
+             'T': 'M',
+             'U': 'T', 
+             'V': '!', 
+             'W': 'G', 
+             'X': 'N', 
+             'Y': 'U',
+             'Z': '?', 
+             '?': 'A', 
+             '@': 'H'}
