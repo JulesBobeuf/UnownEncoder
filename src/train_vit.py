540479f0439858ed7a6ffdccbf694d7a60b52ae6
Bobeuf Jules <jules_bobeuf@ens.univ-artois.fr>
2024-03-02 09:58:10 +0100

    fix : tensors of different sizes

--- src/train_vit.py
+++ src/train_vit.py
@@ -4,19 +4,11 @@ from torch.utils.data import DataLoader
 from torch.nn.utils.rnn import pad_sequence
 import matplotlib.pyplot as plt
 
-def collate_fn(data):
-    print(type(data))
-    print(len(data))
-    tensors, targets = zip(*data)
-    features = pad_sequence(tensors, batch_first=True)
-    targets = torch.stack(targets)
-    return features, targets
-
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device)  
     
-    transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor(), torchvision.transforms.Normalize(0.5, std=0.5)])
+    transform = torchvision.transforms.Compose([torchvision.transforms.Resize((64, 64)), torchvision.transforms.ToTensor(), torchvision.transforms.Normalize(0.5, std=0.5)])
     print(transform) 
     
     train_dataset=torchvision.datasets.ImageFolder(root="./data/", transform=transform)
@@ -24,14 +16,14 @@ if __name__ == "__main__":
     print('Number of train images:', len(test_dataset))
     print('Number of test images:', len(train_dataset))
     
-    train_dataloader = DataLoader(train_dataset, batch_size=4, collate_fn=collate_fn, shuffle=True)
+    train_dataloader = DataLoader(train_dataset, batch_size=4, shuffle=True)
     test_dataloader = DataLoader(test_dataset, shuffle=True)
     print(train_dataloader)
     print(test_dataloader)
     
     examples = enumerate(train_dataloader)
     batch_idx, data = next(examples)
-    print(len(data))
+    print(batch_idx, len(data), type(data))
     
     print('Images:', data[0].shape)
     b, c, h, w = data[0].shape
